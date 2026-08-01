05e660e699dff15bc5560f73345fa50efcc93b11
kodattoria-boop <kodattoria@gmail.com>
2026-08-01 12:01:42 +0900

    Delete kodattoria_site_20260801_1 directory

--- kodattoria_site_20260801_1/lang.js
+++
@@ -1,7 +0,0 @@
-function setLang(lang) {
-  document.body.className = 'lang-' + lang;
-  ['ja','en','it'].forEach(function(l) {
-    var btn = document.getElementById('btn-' + l);
-    if (btn) btn.classList.toggle('active', l === lang);
-  });
-}
